fad6d14e5ab4fce9cccfdd15f50d560ad7c109ee
Jeff Bryan <JBryan5733@gmail.com>
2015-08-19 14:46:43 -0500

    refactoring test file for controllers

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,32 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+	def setup
+		@base_title = "RoR Practice App"
+	end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | RoR Practice App"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | RoR Practice App"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
   	get :about
   	assert_response :success
-  	assert_select "title", "About | RoR Practice App"
+  	assert_select "title", "About | #{@base_title}"
+  end
+
+  test "should get contact" do
+  	get :contact
+  	assert_response :success
+  	assert_select "title", "Contact Us | #{@base_title}"
   end
 
 end
