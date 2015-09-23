74f37a965dbf3c4ecc12f5941cd9b6fa87c66962
Jeff Bryan <JBryan5733@gmail.com>
2015-09-23 13:24:43 -0500

    added user authentication, and secured passwords with bcrypt.

--- Gemfile
+++ Gemfile
@@ -4,7 +4,9 @@ source 'https://rubygems.org'
 
 gem 'rails', 					'4.2.2'
 
-gem 'bootstrap-sass', '~> 3.3.5'
+gem 'bcrypt'					
+
+gem 'bootstrap-sass',           '~>3.3.5'
   			
 gem 'sass-rails', 				'~> 5.0'
 
--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
-class ApplicationHelperTest < ActionView: :TestCase
+class ApplicationHelperTest < ActionView::TestCase
 	test "full title helper" do 
-		assert_equal full_title,		 FILL_IN
-		assert_equal full_title("Help"), FILL_IN
+		assert_equal full_title,		 "RoR Practice App"
+		assert_equal full_title("Help"), "Help | RoR Practice App"
 	end
 end
